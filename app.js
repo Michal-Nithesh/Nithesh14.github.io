456f50d91b4d01cb2c968b1e0a56ef3b63de102a
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-05-16 00:40:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -11,3 +11,19 @@
         document.body.classList.toggle("light-mode");
     })
 })();
+
+// Download button
+document.getElementById('btn-con').addEventListener('click', function() {
+    // Create an anchor element
+    var downloadLink = document.createElement('a');
+    // Set the href attribute to the URL of the file to be downloaded
+    downloadLink.href = '';
+    // Set the download attribute to specify the filename
+    downloadLink.download = 'filename.ext';
+    // Append the anchor element to the body
+    document.body.appendChild(downloadLink);
+    // Trigger a click event on the anchor element
+    downloadLink.click();
+    // Remove the anchor element from the body
+    document.body.removeChild(downloadLink);
+});
