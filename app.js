5fe8799bc6d39b92949f14e6e467bfd710b4af6b
Michal Nithesh <151201789+Nithesh14@users.noreply.github.com>
2024-05-16 07:38:08 +0000

    update

--- app.js
+++ app.js
@@ -38,56 +38,6 @@ document.addEventListener("DOMContentLoaded", function() {
     });
 });
 
-var tl = gsap.timeline();
-tl.from(".letter", { duration: 1, opacity: 0, y: "random(-200, 200)", stagger: 0.15 });
-tl.to(".letter", { duration: 1, rotation: 360, stagger: 0.15 }, "-=3.5");
-
-$(".letter").bind("webkitAnimationEnd mozAnimationEnd animationend", function () {
-    $(this).removeClass("rubberBand");
-});
-
-$(".letter").hover(function () {
-    $(this).addClass("rubberBand");
-});
-
-
-if (!window.matchMedia("(max-width: 1500px)").matches) {
-    gsap.from(".sal", {
-        scrollTrigger: {
-            trigger: ".sal",
-            start: "top bottom"
-        },
-        opacity: 0,
-        x: -200,
-        duration: 1,
-        ease: "power3",
-        stagger: 0.25
-    });
-
-    gsap.from(".sar", {
-        scrollTrigger: {
-            trigger: ".sar",
-            start: "top bottom"
-        },
-        opacity: 0,
-        x: 200,
-        duration: 1,
-        ease: "power3",
-        stagger: 0.25
-    });
-}
-
-gsap.from(".skill-box", {
-    scrollTrigger: {
-        trigger: ".skill-box",
-        start: "top bottom"
-    },
-    opacity: 0,
-    y: 200,
-    duration: 1,
-    ease: "power3",
-    stagger: 0.25
-});
 
 
 //animation
